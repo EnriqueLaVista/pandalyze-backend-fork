bf2fc4891add22eb879928e5517ac44e032d2a9d
tomas <tomas@Ubuntu22new.myguest.virtualbox.org>
2024-02-14 18:26:52 -0300

    Agrego endpoint de ejecucion de codigo python

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 from flask import Blueprint, request, jsonify
 from werkzeug.utils import secure_filename
 
@@ -11,6 +11,29 @@ bp.config = {}
 @bp.route('/')
 def index():
     return 'Index page'
+
+@bp.route('/run_python_code', methods=['POST'])
+def run_python_code():
+    if 'code' not in request.json:
+        return jsonify({'error': 'No se proporcionó ningún código Python'}), 400
+    
+    code = request.json['code']
+    
+    try:
+        # Ejecuta el código Python proporcionado
+        exec_result = {}
+        exec(f"result = {code}", {}, exec_result)
+        
+        # Captura el resultado
+        result = exec_result.get('result', None)
+
+        # Devuelve el resultado como parte de la respuesta JSON
+        if result is not None:
+            return jsonify({'result': result}), 200
+        else:
+            return jsonify({'info': 'No se encontró ningún resultado'}), 500
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
