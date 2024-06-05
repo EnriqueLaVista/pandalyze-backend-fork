7a80e2944331f632cc1b74813acca5de23fe23be
tomas <tomyduggan@gmail.com>
2024-06-05 18:47:47 -0300

    Sumo el archivo config parametrizado

---
+++ app/config.py
@@ -0,0 +1,18 @@
+import os
+
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Determina el entorno segun la variable de entorno FLASK_ENV, o defaultea a development
+    env = os.getenv('FLASK_ENV', 'development')
+
+    # Configuración común
+    DEBUG = True if env == 'development' else False
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_NAME = os.getenv('DB_NAME')
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+def get_config():
+    return Config()
