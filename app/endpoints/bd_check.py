fee5a3683686b93f64c7c25e9f0128a8c41925d5
tomas <tomyduggan@gmail.com>
2024-07-08 20:42:40 -0300

    Bd check extra 2

--- app/endpoints/bd_check.py
+++ app/endpoints/bd_check.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, jsonify
 from app.models.csv_model import CSVData
-from app import db
 
 bp = Blueprint('bd_check', __name__)
 
@@ -10,6 +9,7 @@ def bd_check():
         count = CSVData.query.count()
         return jsonify({'message': 'OK!', 'count': count}), 200
     except Exception as e:
+        from app import db
         db.create_all()
         CSVData.query.all()
         return jsonify({'message': e}), 200
